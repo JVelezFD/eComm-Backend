848a2343df33221a592ddb9d1ba98c8f55387e18
JVelezFD <jvelez117@gmail.com>
2022-08-22 16:13:36 -0400

    added additional models category and product

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoincrement: true,
+    },
+    price: {
+      type: DataTypes.STRING, 
+      allowNull: false, 
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true,
+      }
+    },
+    product_name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      }
+    },
   },
   {
     sequelize,
